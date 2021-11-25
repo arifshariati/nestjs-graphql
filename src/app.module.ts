f7a1d636fe550ee2623076c96d22d23e2621001e
Arif Shariati <mohammad.arif.fast@hotmail.com>
2021-11-25 23:52:07 +0300

    1- Installed required packages (graphql, typeorm, pg) 2- Files cleanup 3- Updated readme.md

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot({ autoSchemaFile: join(process.cwd(),'src/schema.gql')}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'homelike_user',
+      password: 'homelike_password',
+      database: 'homelike',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Backend');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(4000,()=>logger.log('Backend is running on port 4000'));
 }
 bootstrap();
