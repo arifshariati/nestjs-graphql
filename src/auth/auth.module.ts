2149f807a4d3e2b47fee46cef36fc24f2c047408
Arif Shariati <mohammad.arif.fast@hotmail.com>
2021-11-26 20:37:33 +0300

    code refactor auth module and clean up

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,21 +4,23 @@ import { UserModule } from 'src/user/user.module';
 import { devConfig } from 'src/config';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports:[
-    forwardRef(()=> UserModule),
+  imports: [
+    forwardRef(() => UserModule),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-          secret: devConfig.jwt.secret,
-          signOptions: { expiresIn: devConfig.jwt.expiresIn }
+        secret: devConfig.jwt.secret,
+        signOptions: { expiresIn: devConfig.jwt.expiresIn }
       })
-  }),
-  ConfigModule
+    }),
+    ConfigModule
   ],
-  providers: [AuthService],
-  exports:[AuthService]
+  providers: [AuthService, JwtAuthGuard, JwtStrategy],
+  exports: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
